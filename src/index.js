234b92a0c36da5cddbc1e5eda84461e0c04531f9
Shahriyor <shahriyoradhamov@icloud.com>
2024-08-05 11:15:57 +0500

    fix: fix check function

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-    const stack = [];
+function check(str, bracketsConfig) {
+  const stack = [];
   const bracketsMap = new Map(bracketsConfig);
   const openingBrackets = new Set(bracketsConfig.map(pair => pair[0]));
-  const closingBrackets = new Set(bracketsConfig.map(pair => pair[1]));
+  const sameBrackets = bracketsConfig.filter(pair => pair[0] === pair[1]).map(pair => pair[0]);
 
   for (const char of str) {
     if (openingBrackets.has(char)) {
-      // Special case where the bracket can be both opening and closing
-      if (closingBrackets.has(char) && stack.length && stack[stack.length - 1] === char) {
-        stack.pop();
+      if (sameBrackets.includes(char)) {
+        if (stack.length && stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
       } else {
         stack.push(char);
       }
-    } else if (closingBrackets.has(char)) {
-      if (stack.length && stack[stack.length - 1] === bracketsMap.get(char)) {
-        stack.pop();
-      } else {
+    } else {
+      const last = stack.pop();
+      if (bracketsMap.get(char) !== last) {
         return false;
       }
     }
